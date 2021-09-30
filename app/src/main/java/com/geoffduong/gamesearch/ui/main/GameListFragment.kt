9c0207b821fa907b911cd84ba0911964971639b5
Geoff Duong <duongg4848@gmail.com>
2021-09-30 16:55:18 -0400

    Added SearchView to allow for querying the datasource by game name

--- app/src/main/java/com/geoffduong/gamesearch/ui/main/GameListFragment.kt
+++ app/src/main/java/com/geoffduong/gamesearch/ui/main/GameListFragment.kt
@@ -6,9 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
+import androidx.paging.PagingData
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.geoffduong.gamesearch.data.Game
@@ -16,6 +17,7 @@ import com.geoffduong.gamesearch.databinding.ListFragmentBinding
 import com.geoffduong.gamesearch.ui.recyclerview.GameListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onEmpty
 import kotlinx.coroutines.launch
 import java.io.File
 
@@ -36,7 +38,7 @@ class GameListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val viewModel by viewModels<GameViewModel>()
+        val viewModel = ViewModelProvider(requireActivity()).get(GameViewModel::class.java)
 
         val pagingAdapter = GameListAdapter(object : GameListItemOnClickListener {
             override fun onClick(view: View, game: Game?) {
@@ -64,10 +66,13 @@ class GameListFragment : Fragment() {
             DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL)
         )
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.flow.collectLatest { pagingData ->
-                pagingAdapter.submitData(pagingData)
+        viewModel.query.observe(viewLifecycleOwner, {
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewModel.flow.onEmpty { pagingAdapter.submitData(PagingData.empty()) }
+                    .collectLatest { pagingData ->
+                        pagingAdapter.submitData(pagingData)
+                    }
             }
-        }
+        })
     }
 }
--- app/src/main/java/com/geoffduong/gamesearch/ui/main/GameViewModel.kt
+++ app/src/main/java/com/geoffduong/gamesearch/ui/main/GameViewModel.kt
@@ -1,27 +1,46 @@
 package com.geoffduong.gamesearch.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.geoffduong.gamesearch.api.GiantBombService
+import com.geoffduong.gamesearch.data.Game
 import com.geoffduong.gamesearch.ui.recyclerview.GamePagingSource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class GameViewModel() : ViewModel() {
 
     private lateinit var service: GiantBombService
+    private val _query: MutableLiveData<String> = MutableLiveData()
+    val query: LiveData<String> get() = _query
+    var flow: Flow<PagingData<Game>> = emptyFlow()
+
 
     @Inject
     constructor(service: GiantBombService) : this() {
         this.service = service
     }
 
-    val flow =
-        Pager(PagingConfig(20)) {
-            GamePagingSource(service, "name:Planetfall")
-        }.flow.cachedIn(viewModelScope)
+    fun updateQuery(query: String?) {
+        query?.let {
+            val trimmedQuery = query.trim()
+            if (trimmedQuery.isEmpty()) {
+                flow = emptyFlow()
+            } else {
+                flow = Pager(PagingConfig(20)) {
+                    GamePagingSource(service, "name:" + trimmedQuery)
+                }.flow.cachedIn(viewModelScope)
+            }
+            this._query.postValue(query)
+        }
+    }
 }
